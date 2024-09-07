8a62d03ce602024c673c2dc098561b1728a22994
yassine-hg <160339862+yassine-hg@users.noreply.github.com>
2024-09-07 20:26:19 +0100

    Create prictice.sql

---
+++ prictice.sql
@@ -0,0 +1,7 @@
+SELECT advisor_name, advisor_department, advisor_email 
+FROM college 
+WHERE advisor_name = 'Brunson'; 
+
+SELECT major_1, major_1_credits_reqd 
+FROM college 
+WHERE major_1 = 'Computer Science'; 
